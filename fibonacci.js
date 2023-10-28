7bc0b28f4a6d29a37830e3d2f3992df4125df696
sai shravan <saishravan384@gmail.com>
2023-10-28 10:06:33 +0530

    Add error handling to fibs

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,7 @@
 /* Using iteration, write a function fibs which takes a number and returns an array containing that many numbers from the fibonacci sequence. Using an example input of 8, this function should return the array [0, 1, 1, 2, 3, 5, 8, 13]. */
 
 const fibs = (n) => {
+  if (n <= 0) return 'Argument must be atleast 1'
   if (n < 2) return [0]
 
   let result = [0, 1]
